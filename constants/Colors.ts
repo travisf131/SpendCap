f90960f86cbe489fc187e34736bc0905e2051400
travisf131 <travisf131@gmail.com>
2025-06-22 12:55:22 -0700

    Create a global Colors constant for use across components app-wide

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,26 +1,18 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
-
 export const Colors = {
-  light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
-  },
-  dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
-  },
+  text: "#E0E0E0",
+  textSecondary: "#CCCCCC",
+  textTertiary: "#999999",
+  background: "#121212",
+  darkSecondary: "#2A2A2A",
+  dark3: "#222222",
+  dark4: "#1B1B1B",
+  tint: "#fff",
+  icon: "#9BA1A6",
+  tabIconDefault: "#9BA1A6",
+  tabIconSelected: "#fff",
+  colorPlentyLeft: "#4CAF50",
+  colorModerate: "#8BC34A",
+  colorLowFunds: "#FFA133",
+  colorCritical: "#D32F2F",
+  buttonGreen: "#4CAF50",
 };
