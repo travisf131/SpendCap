4c57584638bb6b84a369da30ba62ffee253e4f63
travisf131 <travisf131@gmail.com>
2025-07-20 13:06:17 -0700

    add style prop to pressable icon component

--- components/ui/PressableIcon.tsx
+++ components/ui/PressableIcon.tsx
@@ -1,11 +1,12 @@
 // components/Icon.tsx
 import { MaterialIcons } from "@expo/vector-icons";
-import { Pressable } from "react-native";
+import { Pressable, TextStyle } from "react-native";
 
 type Props = {
   name: React.ComponentProps<typeof MaterialIcons>["name"];
   size?: number;
   color?: string;
+  style?: TextStyle;
   onPress?: () => void;
 };
 
@@ -14,6 +15,7 @@ export default function Icon({
   size = 24,
   color = "white",
   onPress,
+  style,
 }: Props) {
   return (
     <Pressable onPress={onPress}>
@@ -22,7 +24,7 @@ export default function Icon({
           name={name}
           size={size}
           color={pressed ? "#ccc" : color}
-          style={{ opacity: pressed ? 0.6 : 1 }}
+          style={[{ opacity: pressed ? 0.6 : 1 }, style]}
         />
       )}
     </Pressable>
